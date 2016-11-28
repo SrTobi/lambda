90d0a5bd37aaeff508cd163325f6db67abb11921
SrTobi <code.databyte@gmail.com>
2016-11-28 01:57:25 +0100

    improve styling and error reporting

--- src/inputPanel.tsx
+++ src/inputPanel.tsx
@@ -5,13 +5,17 @@ import {Lambda, LambdaFactory} from './lambda';
 import {to_string} from './textPrinter';
 import * as ev from './evaluate';
 import * as utils from './utils';
+import * as Visual from './visuals';
+import * as PegJs from 'pegjs';
 
 declare var require:any;
 import AceEditor from 'react-ace';
 
 
 interface BlockState {
-    evals: Lambda[][];
+    evals?: Lambda[][];
+    error?: string;
+    showAllAppl?: boolean;
 }
 
 class InputBlock extends React.Component<{factory: LambdaFactory, value?: string}, BlockState> {
@@ -25,7 +29,7 @@ class InputBlock extends React.Component<{factory: LambdaFactory, value?: string
         super(props);
         this.code = this.props.value || "";
         this.block = new Block(this.props.factory, undefined, () => {this.onCompile()})
-        this.state = {evals: []}
+        this.state = {evals: [], error: undefined}
     }
 
     private onCompile() {
@@ -37,16 +41,32 @@ class InputBlock extends React.Component<{factory: LambdaFactory, value?: string
         });
     }
 
-    private execute(code: string) {
+    private execute(code: string, editor: any) {
         this.code = code;
-        this.block.setCode(code);
+        this.setState({error: undefined});
+        try {
+            this.block.setCode(code);
+        }catch(e) {
+            let err = e as PegJs.parser.SyntaxError;
+            
+            this.setState({error: err.message + " in line " + err.location.start.line + ", column " + err.location.start.column});
+
+            editor.getSession().setAnnotations([{
+                row: err.location.start.line-1,
+                column: err.location.start.column,
+                text: err.message,
+                type: "error" // also warning and information
+            }]);
+        }
     }
 
     private onSubmit(editor: any) {
-        this.execute(editor.getValue());
+        this.execute(editor.getValue(), editor);
     }
 
     render() {
+        let evals = this.state.evals || [];
+        let showAllAppl = this.state.showAllAppl || false;
         return(
             <div className="input-block">
                 <div className="ace-container">
@@ -57,7 +77,7 @@ class InputBlock extends React.Component<{factory: LambdaFactory, value?: string
                         fontSize={15}
                         className="ace-input-area"
                         minLines={3}
-                        maxLines={20}
+                        maxLines={150}
                         width=""
                         commands={[{
                             name: "submit",
@@ -73,16 +93,28 @@ class InputBlock extends React.Component<{factory: LambdaFactory, value?: string
                             tabSize: 2
                         }}
                         />
-                    </div>
-                <div>
-                    {this.state.evals.map((lmbs) => {
-                        return (
-                            <div>
-                                {lmbs.map((lmb) => <p>{to_string(lmb)}</p>)}
-                            </div>
-                            );
-                    })}
                 </div>
+                <div className="config-box">
+                    <Visual.Switch checked={showAllAppl} onChange={(b:boolean) =>{ this.setState({showAllAppl: b})}} label="Show all steps" />
+                </div>
+                {this.state.error?
+                    <div className="error-box">
+                        Error: {this.state.error}
+                    </div>
+                    :
+                    <div className="output-box">
+                        {evals.map((lmbs) => {
+                            return (
+                                <div className="expr-box">
+                                    {lmbs
+                                        .filter((lmb, idx) => showAllAppl || idx == 0 || idx == lmbs.length - 1)
+                                        .map((lmb, idx) => <div className="appl-box">{idx == 0? "" : "=>"} {to_string(lmb)}</div>)
+                                    }
+                                </div>
+                                );
+                        })}
+                    </div>
+                }
             </div>
         );
     }
--- src/visuals.tsx
+++ src/visuals.tsx
@@ -64,3 +64,26 @@ export class NumberForm extends React.Component<{value: number, step?: number, m
 		);
 	}
 }
+
+
+export class Switch extends React.Component<{checked: boolean, label: string, id?: string, onChange: (checked: boolean) => void}, {}> {
+
+	private input: HTMLInputElement;
+	private id = this.props.id || Utils.randomString();
+
+	private onChange(): void {
+		this.props.onChange(this.input.checked);
+	}
+
+	render() {
+		return (
+			<div className="switch-grouper">
+                <label htmlFor={this.id}>{this.props.label}</label>
+				<label className="switch">
+					<input type="checkbox" checked={this.props.checked} id={this.id} ref={e => this.input = e} onChange={this.onChange.bind(this)} />
+					<div className="slider"></div>
+				</label>
+			</div>
+		);
+	}
+}
