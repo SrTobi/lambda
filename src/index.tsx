5f81dca6ebe3012a9555c421a14a058a6f7885e0
SrTobi <code.databyte@gmail.com>
2017-09-17 22:36:55 +0200

    removed settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {Settings, SettingsPanel} from './settings';
 import {InputPanel} from './inputPanel';
 import {LambdaFactory} from './lambda';
 import * as $ from "jquery";
